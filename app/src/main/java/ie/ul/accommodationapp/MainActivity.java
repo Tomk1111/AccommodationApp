d95017cdc9bac4c8f610edf81f09eb7634825db4
seanmorrissey98 <35046602+seanmorrissey98@users.noreply.github.com>
2020-03-04 13:15:38 +0000

    Added firebase authentication and all blank fragments.

--- app/src/main/java/ie/ul/accommodationapp/MainActivity.java
+++ app/src/main/java/ie/ul/accommodationapp/MainActivity.java
@@ -2,13 +2,69 @@ package ie.ul.accommodationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickSignIn(View view) {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build()
+        );
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(), RC_SIGN_IN);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode == RC_SIGN_IN){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if(resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Context context = getApplicationContext();
+                CharSequence text = "Sign in Successful. Welcome " + user.getDisplayName()+ "!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            } else {
+                if(response == null){
+                    return;
+                }
+                if(response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Context context = getApplicationContext();
+                    CharSequence text = "No Internet Connection.";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    return;
+                }
+            }
+        }
     }
 }
