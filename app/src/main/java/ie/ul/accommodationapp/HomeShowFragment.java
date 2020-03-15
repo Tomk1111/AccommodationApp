022c325895367dae0377579128fdc1e20cac6fd3
paulmurphy291 <paulmurphy291@gmail.com>
2020-03-15 20:18:02 +0000

    -Added functionality to display listings on home page -Basic Appearance for now will improve later

--- app/src/main/java/ie/ul/accommodationapp/HomeShowFragment.java
+++ app/src/main/java/ie/ul/accommodationapp/HomeShowFragment.java
@@ -4,18 +4,28 @@ package ie.ul.accommodationapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeShowFragment extends Fragment {
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference notebookRef = db.collection("Listings");
 
-
+    private ListAdapter adapter;
     public HomeShowFragment() {
         // Required empty public constructor
     }
@@ -25,7 +35,29 @@ public class HomeShowFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home_show, container, false);
+        Query query = notebookRef.orderBy("price", Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Listing> options = new FirestoreRecyclerOptions.Builder<Listing>()
+                .setQuery(query, Listing.class)
+                .build();
+        adapter = new ListAdapter(options);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
         return view;
     }
 
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
 }
