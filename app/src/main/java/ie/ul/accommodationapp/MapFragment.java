62266e84ba9b3e5203c408018deb4f7cbb15582a
Paul Murphy <paulmurphy291@gmail.com>
2020-03-17 14:09:42 +0000

    -Added functionally to display google maps fragment -Displays marker with address of location for each house listing -ToDo-add location of current user and zoom in on their marker on the map

--- app/src/main/java/ie/ul/accommodationapp/MapFragment.java
+++ app/src/main/java/ie/ul/accommodationapp/MapFragment.java
@@ -1,31 +1,122 @@
 package ie.ul.accommodationapp;
-
-
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Map;
+
+import static com.firebase.ui.auth.AuthUI.TAG;
+
+
 public class MapFragment extends Fragment {
-
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     public MapFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.frg);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(final GoogleMap mMap) {
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+                mMap.clear(); //clear old markers
+                FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
+                CollectionReference pointsRef = rootRef.collection("Listings");
+                pointsRef.get()
+                        .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                            @Override
+                            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                for(QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+                                    Listing listing = documentSnapshot.toObject(Listing.class);
+                                    String name = listing.getAddress();
+                                    double lat = listing.getLatitude();
+                                    double lng = listing.getLongitude();
+                                    LatLng latLng = new LatLng(lat, lng);
+                                    mMap.addMarker(new MarkerOptions().position(latLng).title(name));
+                                }
+                            }
+                        });
+
+//                CameraPosition googlePlex = CameraPosition.builder()
+//                        .target(new LatLng(37.4219999,-122.0862462))
+//                        .zoom(10)
+//                        .bearing(0)
+//                        .tilt(45)
+//                        .build();
+//
+//                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null);
+//
+//                mMap.addMarker(new MarkerOptions()
+//                        .position(new LatLng(37.4219999, -122.0862462))
+//                        .title("Spider Man"));
+//
+//                mMap.addMarker(new MarkerOptions()
+//                        .position(new LatLng(37.4629101,-122.2449094))
+//                        .title("Iron Man")
+//                        .snippet("His Talent : Plenty of money"));
+//
+//                mMap.addMarker(new MarkerOptions()
+//                        .position(new LatLng(37.3092293,-122.1136845))
+//                        .title("Captain America"));
+            }
+        });
+
+
+        return rootView;
+    }
+
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 
 }
