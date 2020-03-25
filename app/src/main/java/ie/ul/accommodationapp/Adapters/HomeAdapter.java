f61663980edb7c87d03546db9c229bee839cd288
tom <tomk.tk53@gmail.com>
2020-03-25 21:58:53 +0000

    Search Functionality Added

--- app/src/main/java/ie/ul/accommodationapp/Adapters/HomeAdapter.java
+++ app/src/main/java/ie/ul/accommodationapp/Adapters/HomeAdapter.java
@@ -1,4 +1,115 @@
 package ie.ul.accommodationapp.Adapters;
 
-public class HomeAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ie.ul.accommodationapp.Listing;
+import ie.ul.accommodationapp.R;
+
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder> implements Filterable {
+
+    private List<Listing> mData;
+    private List<Listing> mDataFiltered;
+    private Context mContext;
+
+    public HomeAdapter(Context mContext, List<Listing> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+        this.mDataFiltered = mData;
+    }
+
+
+    @NonNull
+    @Override
+    public HomeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.note_item, parent, false);
+        return new HomeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeViewHolder holder, int position) {
+        holder.setTitleText(mDataFiltered.get(position).getPrice() + "");
+        holder.setDescriptionTextView(mDataFiltered.get(position).getDescription());
+        holder.setPriorityTextView(mDataFiltered.get(position).getAddress());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataFiltered.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                String key = constraint.toString();
+                if (key.isEmpty()) {
+                    mDataFiltered = mData;
+                } else {
+                    List<Listing> listFiltered = new ArrayList<>();
+                    for (Listing listingRow : mData) {
+                        if (listingRow.getAddress().toLowerCase().contains(key.toLowerCase())
+                                || listingRow.getDescription().contains(key.toLowerCase())) {
+                            listFiltered.add(listingRow);
+                        }
+                    }
+                    mDataFiltered = listFiltered;
+                }
+
+
+
+
+
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values=mDataFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                mDataFiltered = (List<Listing>) results.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    public class HomeViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView titleTextView, descriptionTextView, priorityTextView;
+
+        public HomeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.text_view_title);
+            descriptionTextView = itemView.findViewById(R.id.text_view_description);
+            priorityTextView = itemView.findViewById(R.id.text_view_priority);
+
+        }
+
+        public void setTitleText(String text) {
+            titleTextView.setText(text);
+        }
+
+        public void setDescriptionTextView(String text) {
+            descriptionTextView.setText(text);
+        }
+
+        public void setPriorityTextView(String text) {
+            priorityTextView.setText(text);
+        }
+    }
+
+
 }
--- app/src/main/java/ie/ul/accommodationapp/SearchFragment.java
+++ app/src/main/java/ie/ul/accommodationapp/SearchFragment.java
@@ -2,18 +2,44 @@ package ie.ul.accommodationapp;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import ie.ul.accommodationapp.Adapters.HomeAdapter;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SearchFragment extends Fragment {
+
+    private EditText searchText;
+    private RecyclerView recyclerView;
+    private HomeAdapter homeAdapter;
+    private List<Listing> mData;
+    private FirebaseFirestore firebaseFirestore;
+    private CollectionReference collectionReference;
+    private ListAdapter adapter;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference notebookRef = db.collection("Listings");
 
 
     public SearchFragment() {
@@ -24,8 +50,65 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        final View view = inflater.inflate(R.layout.fragment_search, container, false);
+        searchText = view.findViewById(R.id.search_box);
+        recyclerView = view.findViewById(R.id.house_listing_recycler_view);
+        retrieveListings();
+        searchText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                homeAdapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        return view;
     }
+
+
+
+
+
+    public void retrieveListings() {
+
+
+        mData = new ArrayList<>();
+        mData.add(new Listing(11111, 232, 223, "aaa", 2, 23, "test", new Date(),
+                new Date(),  200, "a"));
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference collectionReference = db.collection("Listings");
+        collectionReference.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if(!queryDocumentSnapshots.isEmpty()) {
+                            List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                            for (DocumentSnapshot documentSnapshot : list) {
+                                Listing listing = documentSnapshot.toObject(Listing.class);
+                                mData.add(listing);
+                            }
+                        }
+
+                        homeAdapter = new HomeAdapter(getContext(), mData);
+
+                        recyclerView.setAdapter(homeAdapter);
+                        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+                        linearLayoutManager.setStackFromEnd(true);
+                        linearLayoutManager.setReverseLayout(true);
+                        recyclerView.setLayoutManager(linearLayoutManager);
+                    }
+                });
+
+
+    }
+
 
 }
