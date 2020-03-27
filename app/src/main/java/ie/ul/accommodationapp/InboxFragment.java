26cead5b9569de546d0ec6340fa7f7eb12afea50
tom <tomk.tk53@gmail.com>
2020-03-27 11:49:58 +0000

    Revert "Added objects to store Firebase DB data."
    This reverts commit 5e1b05cfde24c9c7d58e69876207798bfa1fde5e.

--- app/src/main/java/ie/ul/accommodationapp/InboxFragment.java
+++ app/src/main/java/ie/ul/accommodationapp/InboxFragment.java
@@ -1,43 +1,13 @@
 package ie.ul.accommodationapp;
 
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.firebase.ui.database.SnapshotParser;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-
-import de.hdodenhof.circleimageview.CircleImageView;
-
-import static com.firebase.ui.auth.AuthUI.TAG;
 
 
 /**
@@ -46,124 +16,16 @@ import static com.firebase.ui.auth.AuthUI.TAG;
 public class InboxFragment extends Fragment {
 
 
-
     public InboxFragment() {
         // Required empty public constructor
     }
 
-    //private static final String TAG = "MainActivity";
-    public static final String MESSAGES_CHILD = "messages";
-    private static final int REQUEST_INVITE = 1;
-    private static final int REQUEST_IMAGE = 2;
-    private static final String LOADING_IMAGE_URL = "https://www.google.com/images/spin-32.gif";
-    public static final int DEFAULT_MSG_LENGTH_LIMIT = 10;
-    public static final String ANONYMOUS = "anonymous";
-    private static final String MESSAGE_SENT_EVENT = "message_sent";
-    private String mUsername;
-    private String mPhotoUrl;
-    private SharedPreferences mSharedPreferences;
-    private GoogleApiClient mGoogleApiClient;
-    private static final String MESSAGE_URL = "http://friendlychat.firebase.google.com/message/";
-
-    private Button mSendButton;
-    private RecyclerView mMessageRecyclerView;
-    private LinearLayoutManager mLinearLayoutManager;
-    private ProgressBar mProgressBar;
-    private TextInputEditText mMessageEditText; //EditText
-    private ImageView mAddMessageImageView;
-
-    //*added
-    // Firebase instance variables
-    private FirebaseAuth mFirebaseAuth;
-    private FirebaseUser mFirebaseUser;
-    private DatabaseReference mFirebaseDatabaseReference;
-
-    private FirebaseDatabase database;
-    private DatabaseReference myRef;
-
-    ///
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)  {
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mFirebaseUser = mFirebaseAuth.getCurrentUser();
-        System.out.println("MARK111");
-        System.out.println("USER DATA"+mFirebaseUser.getDisplayName() + " " + mFirebaseUser.getEmail());
-
-
-        // Write a message to the database
-            FirebaseDatabase database = FirebaseDatabase.getInstance();
-            myRef = database.getReference("message");
-
-        //myRef.setValue("Mary","Hello");
-
-        /*
-        Query reference=FirebaseDatabase.getInstance().getReference().child("messages"); //.orderByChild("name").equalTo("iphon")
-        reference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
-                    String key = childSnapshot.getKey();
-                    //Log.i(TAG,key);
-                    System.out.println(key);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-        */
-
-        ///ALTERNATE WAY TO CALL FIREBASE DB
-/*
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                System.out.println("MARKS HERE: "+ dataSnapshot.toString());
-                System.out.println("MARKS HERE: "+ dataSnapshot.getKey() );
-
-                for (DataSnapshot childSnapshot: dataSnapshot.getChildren()) {
-                    String key=childSnapshot.getKey();
-                    //Log.i(TAG,key);
-                    System.out.println(key);
-                }
-
-                //Message value = dataSnapshot.getValue(Message.class);
-                //System.out.println("Value is: " + value.toString());
-                //Log.d(TAG, value.toString());
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                System.out.println("Failed");
-            }
-        });
-
-*/
-
-
-
-
-
         return inflater.inflate(R.layout.fragment_inbox, container, false);
-
-
-    }
-
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-
     }
 
 }
