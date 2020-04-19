66ea43019d11ab83a361fe14f029d5334c70932d
mcnabola <17226163@studentmail.ul.ie>
2020-04-19 04:58:37 +0100

    chatfragment basic styled and navigation between inbox and chat created

--- app/src/main/java/ie/ul/accommodationapp/ChatFragment.java
+++ app/src/main/java/ie/ul/accommodationapp/ChatFragment.java
@@ -7,9 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import static com.firebase.ui.auth.AuthUI.getApplicationContext;
 
 
 /**
@@ -20,6 +23,8 @@ public class ChatFragment extends Fragment {
     private Toolbar mToolbar;
     private View MessageView;
     private FirebaseAuth mAuth;
+    private String messageReceiverID;
+    private String messageReceiverName;
 
     public ChatFragment() {
         // Required empty public constructor
@@ -31,6 +36,19 @@ public class ChatFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //get bundle values in onCreate not onCreateView
+        Bundle chatBundle = this.getArguments();
+        //Data validation on the bundle to check whether data was passed or not.
+        if (chatBundle != null) {
+            messageReceiverID = chatBundle.getString("uid");
+            messageReceiverName = chatBundle.getString("userName");
+            //need to get one of the users house images too if one exists
+            // need to find out if an account has already been created for this user - if from inbox they definitely have
+        }
+        System.out.println("userid " + messageReceiverID);
+        System.out.println("username " + messageReceiverName);
+        Toast.makeText(getActivity(), messageReceiverID, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), messageReceiverName, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -40,7 +58,7 @@ public class ChatFragment extends Fragment {
         MessageView = inflater.inflate(R.layout.fragment_chat, container, false);
 
 
-        //needed for the toolbar
+        /*//needed for the toolbar
         mToolbar = getActivity().findViewById(R.id.main_toolbar);
         mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
         mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -48,7 +66,7 @@ public class ChatFragment extends Fragment {
             public void onClick(View v) {
                 getActivity().onBackPressed();
             }
-        });
+        });*/
 
         return MessageView;
     }
